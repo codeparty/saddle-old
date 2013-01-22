a8ae741ad7d1865c2b148eeb4b00bd6235d73649
rma4ok <rma4ok@gmail.com>
2013-01-22 16:44:11 -0500

    [test] preparing testacular

---
+++ testacular.conf.js
@@ -0,0 +1,65 @@
+// Testacular configuration
+// Generated on Tue Jan 22 2013 16:13:12 GMT-0500 (EST)
+
+
+// base path, that will be used to resolve files and exclude
+basePath = '../../../../../..';
+
+
+// list of files / patterns to load in the browser
+files = [
+  MOCHA,
+  MOCHA_ADAPTER
+];
+
+
+// list of files to exclude
+exclude = [
+  
+];
+
+
+// test results reporter to use
+// possible values: 'dots', 'progress', 'junit'
+reporters = ['progress'];
+
+
+// web server port
+port = 8080;
+
+
+// cli runner port
+runnerPort = 9100;
+
+
+// enable / disable colors in the output (reporters and logs)
+colors = true;
+
+
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+logLevel = LOG_INFO;
+
+
+// enable / disable watching file and executing tests whenever any file changes
+autoWatch = true;
+
+
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari (only Mac)
+// - PhantomJS
+// - IE (only Windows)
+browsers = ['Chrome'];
+
+
+// If browser does not capture in given timeout [ms], kill it
+captureTimeout = 5000;
+
+
+// Continuous Integration mode
+// if true, it capture browsers, run tests and exit
+singleRun = false;
