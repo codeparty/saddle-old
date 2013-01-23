54067abd676e126afc1634527894301bec72c357
rma4ok <rma4ok@gmail.com>
2013-01-23 02:31:06 -0500

    [test] raise browser kill timeout

--- testacular.conf.js
+++ testacular.conf.js
@@ -59,7 +59,7 @@ browsers = ['Chrome', 'Firefox', 'Opera', 'Safari'];
 
 
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+captureTimeout = 10000;
 
 
 // Continuous Integration mode
